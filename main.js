52f8d8eeb70ff7e3e15adb6d952bbf45ada90ba9
max1755 <112920950+max1755@users.noreply.github.com>
2024-04-01 15:43:39 -0400

    cleaned

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ function createWindow(){ //Electron includes this browser window api to create a
         //In here we can adjust the size of our window, as well as other properties such as customizing or hiding the toolbar...
         width: 800,
         height: 600,
-        resizable: true
+        resizable: true,
+        //Tool bar
+        //hiding the tool bar
+        //frame: false
+        //titleBarStyle: "hidden"
+        
     });
     win.loadURL(url.format({
         pathname: path.join(__dirname, "index.html"),
@@ -25,6 +30,15 @@ function createWindow(){ //Electron includes this browser window api to create a
         win.on('closed', () => { //<== this is an event that will run when the program window is closed
             win = null; //garbage collecting
         })
+
+        //glow for attention
+        setTimeout(() => { 
+            win.flashFrame(true); 
+        }, 2000) 
+          
+        win.once('focus', () => win.flashFrame(false)); 
+
+        
 }
 
 //We can use the app module to create the window
